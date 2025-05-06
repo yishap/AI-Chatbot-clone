5ac45f2645ffe2aa4b3ddcef0599d3627cd9513b
yishap <104184446+yishap@users.noreply.github.com>
2025-05-06 08:34:19 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./components/Main/Main"
+import Sidebar from "./components/sidebar/sidebar"
+
+const App = () => {
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+    </>
+  )
+}
+
+export default App
